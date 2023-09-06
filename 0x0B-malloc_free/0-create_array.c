1e10165f227aefed63be3e2d2577c19db36300ba
Hulritch <hulritchbulobo2@gmail.com>
2023-09-06 06:15:51 -0700

    new array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,7 +8,7 @@ char *create_array(unsigned int size, char c)
 	if(size == 0)
 		return(NULL);
 
-	char *s = (char*)malloc(sizeof(char)*size);
+ *s = (char*)malloc(sizeof(char)*size);
 
 	for (i = 0; i < size; i++)
 		s[i] = c;
