3ed351634418fe4220a4cf22300fe980a90c3b99
Hulritch <hulritchbulobo2@gmail.com>
2023-09-20 06:50:14 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,28 +1,25 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog - dopgs info
+ *@name - first element 
+ *@age - second element
+ *@owner - third element 
+*/
+
 struct dog
 {
-char name;
+char *name;
 float age;
-char owner;
+char *owner;
 };
 
-int main()
-{
-struct dog my_dog;
-
-my_dog.name = "bobby";
-my_dog.age = 45;
-my_dog.owner = "booty";
-return (0);
-}
-
-
+typedef struct dog dog_my;
 char _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-char dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
